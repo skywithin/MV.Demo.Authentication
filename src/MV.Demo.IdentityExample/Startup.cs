8019a69864c5f126b35a60b24d1d062e2080b6c5
Max <sydneymax@gmail.com>
2020-09-02 09:09:06 +1000

    Add in-memory database

--- src/MV.Demo.IdentityExample/Startup.cs
+++ src/MV.Demo.IdentityExample/Startup.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MV.Demo.IdentityExample.Data;
 
 namespace MV.Demo.IdentityExample
 {
@@ -14,6 +16,11 @@ namespace MV.Demo.IdentityExample
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(config =>
+            {
+                config.UseInMemoryDatabase("InMemoryDB");
+            });
+
             //services.AddAuthentication("CookieAuth")
             //        .AddCookie("CookieAuth", config =>
             //        {
